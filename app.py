5d6340b6ca7005dbe8bcc03e29fee50cfe411841
YiMeng-Beijing <32533585+MatthewmengBJ@users.noreply.github.com>
2026-03-30 14:51:13 -0400

    1st update
    Enable home page to display empty drop-down list.

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "<h2>Hello Yi Meng!</h2><p>Your Jira PoC tool is running.</p>"
+    return render_template("index.html", project=[])
 
 if __name__ == "__main__":
     app.run()
